67bc6ef55f65254e4399d48c43c7249ecbca308d
MitasuTomiyama <m.tomiyama52@gmail.com>
2020-04-19 11:47:00 +0900

    [Update] favorited_by?

--- app/models/art.rb
+++ app/models/art.rb
@@ -8,4 +8,10 @@ class Art < ApplicationRecord
 
     #Commentモデルの親
     has_many :comments, dependent: :destroy
+
+
+    def favorited_by?(user)
+        favorites.where(user_id: user.id).exists?
+    end
+
 end
