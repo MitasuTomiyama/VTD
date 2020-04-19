25a4f75423e01228e1043918b049e1ae8106e1bc
MitasuTomiyama <m.tomiyama52@gmail.com>
2020-04-19 12:06:32 +0900

    [Update] Favorite_Controller

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,10 +1,18 @@
 class FavoritesController < ApplicationController
   def index
+    @user = current_user
+    @favorites = @user.favorites.art.all
   end
 
   def create
+    favorite = current_user.favorites.build(art_id: params[:art_id])
+    favorite.save
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
+    favorite = Favorite.find_by(art_id: params[:art_id], user_id: current_uset.id)
+    favorite.destroy
+    redirect_back(fallback_location: root_path)
   end
 end
