1ce4eed5e4944a39b94e464465492a44a2c1be30
MitasuTomiyama <m.tomiyama52@gmail.com>
2020-04-19 11:39:04 +0900

    [Update] User_Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,33 @@
 class UsersController < ApplicationController
-  def new
-  end
-
+  
   def index
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
-  end
-
-  def create
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # flash[:notice] =
+      redirect_to user_path(@user)
+    else
+      @user = @user
+      render action: :edit
   end
 
   def destroy
+    @user = current_user.id
+    @user.destroy
+    redirect_to users_path
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :image_id)
 end
