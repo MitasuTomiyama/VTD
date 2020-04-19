28af79de30a2fd93a99424b75b29ca7ccaa1ac89
MitasuTomiyama <m.tomiyama52@gmail.com>
2020-04-19 16:18:57 +0900

    [Update] Comment_Controller & Comment_Model

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,35 @@
 class CommentsController < ApplicationController
 
   def index
+    @art = Art.find(params[:id])
+    @comments = @art.comment.all
   end
 
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to art_path(@commnet.art_id)
+    else
+      redirect_to art_path(@comment.art_id)
   end
 
   def update
+    @comment = Comment.find(:id)
+    if @comment.update(comment_params)
+      redirect_back(fallback_location: root_path[:@comment.art_id])
+    else
+      redirect_back(fallback_location: root_path[:@comment.art_id])
   end
 
   def destroy
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      redirect_back(fallback_location: root_path[:@comment.art_id])
+    else
+      redirect_back(fallback_location: root_path[:@comment.art_id])
   end
+
+  private
+  def comment_params
+    params.require(:comment),parmit(:user_id, :art_id, :comment_id, :comment)
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,16 +1,16 @@
 class Comment < ApplicationRecord
 
 
-    #親comment
-    has_many :comments, dependent: :destroy
+    #親comment=>子commentモデル
+    has_many :replies, class_name: 'Comment', foreign_key: 'comment_id', dependent: :destroy
+
+    #親comment<=子commentモデル
+    belongs_to :original_comment, class_name: 'Comment', foreign_key: 'comment_id', optional: true
 
     #Userの子モデル
     belongs_to :user
 
     #artの子モデル
     belongs_to :art
-
-    #親commentの子モデル
-    belongs_to :comment
 
 end
